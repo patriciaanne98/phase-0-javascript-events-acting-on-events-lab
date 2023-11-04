3509a1272fe30868f94c777b0989c0a546ca41c7
Patricia Frogoso <pfrogoso@gmail.com>
2023-11-04 14:15:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight() {
+    const newPosition = dodger.style.left.replace("px", "");
+    const left = parseInt(newPosition, 10);
+  
+    if (left > 0 ) {
+      dodger.style.left = `${left + 1}40px`;
+    }
+  }
